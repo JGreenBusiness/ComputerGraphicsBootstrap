253b5faa9f2623c3d3e7326b4fed840a5a315aeb
Justin Green <98858930+JGreenBusiness@users.noreply.github.com>
2023-03-14 14:46:26 +1100

    Created mesh class

--- ComputerGraphics/ComputerGraphicsApp.h
+++ ComputerGraphics/ComputerGraphicsApp.h
@@ -19,20 +19,9 @@ public:
 	virtual void draw();
 
 protected:
-	void DrawPlanet(glm::vec3 _pos, float _radius, glm::vec4 _colour);
-
-	glm::mat4 RotateAround(float radius, const glm::vec3& point, const glm::vec3& axis);
 
 	// camera transforms
 	glm::mat4	m_viewMatrix;
 	glm::mat4	m_projectionMatrix;
-
-	std::vector<float> m_planetSizes;
-
-	std::vector <glm::vec3> m_planetPos;
-
-	std::vector <glm::vec4> m_planetColours;
-
-	const glm::mat4* m_mat;
 
 };
--- ComputerGraphics/Planetoid.cpp
+++ ComputerGraphics/Planetoid.cpp
@@ -14,7 +14,16 @@ Planetoid::~Planetoid()
 
 void Planetoid::Update(float _dt)
 {
-	
+
+	//aie::Gizmos::addSphere(glm::vec3(0), 1, 15, 15, glm::vec4(1, 0, 0, 0.5f));
+
+
+	//glm::mat4 mercury = glm::mat4(1);
+
+	//float time = aie::getTime();
+	//mercury = glm::translate(mercury, glm::vec3(glm::sin(time * 1.0f), 0, glm::cos(time * 1.0f)));
+
+	//aie::Gizmos::addSphere(glm::vec3(0), .5, 12, 12, glm::vec4(1), &mercury);
 }
 
 void Planetoid::Draw()
